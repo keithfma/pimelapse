d331912b68f35dd3d0eff87d6a0338b1f2e54d27
Keith Ma <keithfma@gmail.com>
2019-08-18 01:44:25 -0400

    Implements pimelapse-video CLI

--- pimelapse.py
+++ pimelapse.py
@@ -4,7 +4,10 @@ import warnings
 import boto3
 from io import BytesIO
 from time import sleep
-from datetime import datetime
+from datetime import datetime, timedelta
+from tempfile import TemporaryDirectory
+import os
+import subprocess
 
 
 logging.basicConfig(level=logging.INFO)
@@ -47,10 +50,65 @@ def images_cli(interval_min):
 
 
 @click.command()
-def video_cli():
+@click.option('-d', '--days', type=float, required=False, help='days to include in video interval')
+@click.option('-h', '--hours', type=float, required=False, help='hours to include in video interval')
+@click.option('-m', '--minutes', type=float, required=False, help='minutes to include in video interval')
+@click.option('-f', '--framerate', type=int, required=False, default=30,
+              help='output video framerate, in images per second')
+@click.option('-o', '--output', type=str, required=False, default='video',
+              help='Name (without extension) for video file to create')
+def video_cli(days, hours, minutes, framerate, output):
     """Build video from time-lapse images on S3"""
-    # TODO: initialize access to S3
-    # TODO: find images in time interval
-    # TODO: sync image folder
-    # TODO: build video
-    raise NotImplementedError()
+    if not days and not hours and not minutes:
+        raise ValueError('0 duration specified, aborting')
+    duration = timedelta(seconds=0)
+    if days:
+        duration += timedelta(days=days)
+    if hours:
+        duration += timedelta(hours=hours)
+    if minutes:
+        duration += timedelta(minutes=minutes)
+    start_time = datetime.now() - duration
+    log.info(f'Creating video from images starting at {start_time} with total duration of {duration}')
+
+    log.info('Initialize S3 access')
+    client = boto3.client('s3') 
+
+    # find images
+    image_keys = []
+    response = client.list_objects_v2(Bucket=IMAGE_BUCKET, Prefix=f'{IMAGE_PREFIX}/')
+    image_keys.extend(c['Key'] for c in response['Contents'])
+    while response['IsTruncated']:
+        response = client.list_objects_v2(
+            Bucket=IMAGE_BUCKET, Prefix=IMAGE_PREFIX, ContinuationToken=response['NextContinuationToken'])
+        image_keys.extend(c['Key'] for c in response['Contents'])
+
+    # find subset in time interval
+    image_keys = sorted(image_keys)
+    valid_image_keys = []
+    for image_key in image_keys:
+        try:
+            image_date_str = os.path.splitext(os.path.basename(image_key))[0]
+            image_date = datetime.strptime(image_date_str, TIME_FORMAT)
+        except ValueError:
+            log.warning(f'Skipped key: {image_key}')
+            continue
+        if image_date >= start_time:
+            valid_image_keys.append(image_key)
+    log.info(f'Found {len(valid_image_keys)} images in time window')
+
+    with TemporaryDirectory() as tmp_dir:
+        # download image
+        for ii, image_key in enumerate(valid_image_keys):
+            image_path = os.path.join(tmp_dir, os.path.basename(image_key))
+            log.info(f'Downloading {ii+1}/{len(valid_image_keys)}: {image_path}') 
+            response = client.get_object(Bucket=IMAGE_BUCKET, Key=image_key)
+            with open(image_path, 'wb') as fp:
+                fp.write(response['Body'].read())
+        
+        # generate video
+        output_file = f'{output}.mp4'
+        cmd = ['ffmpeg', '-y', '-framerate', f'{framerate}', '-i', f'{tmp_dir}/%*.png', output_file]
+        subprocess.run(cmd, check=True)
+        log.info(f'Output video created at {output_file}')
+
